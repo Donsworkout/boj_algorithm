eb5783813949302ce25bcda2d5d795ac5714e5a0
donkim <dickings@likelion.org>
2018-12-30 17:32:00 +0900

    10869 사칙연산 : 특이사항 : 1.5분컷

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,7 +4,8 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../10817.cpp \
+../boj_10817.cpp \
+../boj_10869.cpp \
 ../boj_1107.cpp \
 ../boj_11719.cpp \
 ../boj_1436.cpp \
@@ -16,7 +17,8 @@ CPP_SRCS += \
 ../숫자의합.cpp 
 
 OBJS += \
-./10817.o \
+./boj_10817.o \
+./boj_10869.o \
 ./boj_1107.o \
 ./boj_11719.o \
 ./boj_1436.o \
@@ -28,7 +30,8 @@ OBJS += \
 ./숫자의합.o 
 
 CPP_DEPS += \
-./10817.d \
+./boj_10817.d \
+./boj_10869.d \
 ./boj_1107.d \
 ./boj_11719.d \
 ./boj_1436.d \
---
+++ boj_10869.cpp
@@ -0,0 +1,18 @@
+/*
+// #10869 사칙연산
+#include <iostream>
+using namespace std;
+
+int main(){
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int a=0; int b=0;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	return 0;
+}
+*/
